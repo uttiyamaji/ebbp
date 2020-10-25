dd7aa088356e66b70d70d46cb2a671bdd3bed598
uttiyamaji <iuttiya@gmail.com>
2020-10-26 00:16:06 +0530

    fixes

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 
 
 """package __init_()"""
+
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,16 @@
 
 
 """ adding __init__() for making a directory"""
+
+
+from src.ebb_fit_prior import ebb_fit_prior, augment
+from src.add_ebb_estimate import add_ebb_estimate
+
+
+
+# #ebb_fit_prior()
+
+
+
+#import ebb_fit_prior
+#import add_ebb_estimate
