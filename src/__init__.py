e0cf4728c4e3539b2481b719de338cd6e78cb78e
uttiyamaji <iuttiya@gmail.com>
2020-11-02 01:02:06 +0530

    random

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,12 @@
 """ adding __init__() for making a directory"""
 
 
-from ebbp.src.ebb_fit_prior import ebb_fit_prior, augment
-from ebbp.src.add_ebb_estimate import add_ebb_estimate
+#from src.ebb_fit_prior import ebb_fit_prior, augment
+#from src.add_ebb_estimate import add_ebb_estimate
 
-
+# tinygrad style imports 
+#import src.ebb_fit_prior
+#import src.add_ebb_estimate
 
 # #ebb_fit_prior()
 
