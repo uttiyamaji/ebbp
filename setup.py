eb07a9dc4157343a931d0486995494a1291b1880
uttiyamaji <iuttiya@gmail.com>
2020-11-08 01:58:32 +0530

    description

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ Created on Sun Nov  1 17:39:15 2020
 
 import setuptools
 
-with open("README.md","r") as fh:
-    long_description = fh.read()
+#with open("README.md","r") as fh:
+ #   long_description = fh.read()
     
 setuptools.setup(
     name = "ebbp",
@@ -17,8 +17,8 @@ setuptools.setup(
     author = "Uttiya Maji",
     author_email = "iuttiya@gmail.com",
     description = "Empirical Bayes for the Binomial in Python",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
+  #  long_description = long_description,
+   # long_description_content_type = "text/markdown",
     url = "https://github.com/uttiyamaji/ebbp",
     packages = setuptools.find_packages(),
     classifiers = ["Programming Language :: Python :: 3",
